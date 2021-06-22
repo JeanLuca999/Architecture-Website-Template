a08f3f375886d248726822e33f860d3e7b917bd9
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-06-22 09:09:32 -0300

    creating mobile close button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 const menuButton = document.getElementsByClassName('menu')[0]
 const menuHidden = document.getElementById('menu-hidden')
+const closeButton = document.getElementById('close-button')
 
 menuButton.addEventListener('click', () => {
+    menuHidden.classList.toggle('menu-open')
+})
+
+closeButton.addEventListener('click', () => {
     menuHidden.classList.toggle('menu-open')
 })
